b41f7f60d82b485b1bc2a65abc8514bc4004790e
Philippa Johnstone <pecj@hotmail.co.uk>
2015-11-19 23:34:09 +0800

    in class mocha testing intro

--- app/test/candies_tests.js
+++ app/test/candies_tests.js
@@ -1,16 +1,15 @@
 var should    = require("chai").should();
 var expect    = require("chai").expect;
-var supertest = require("supertest")
+var supertest = require("supertest");
 
-api           = supertest("http://localhost:3000")
+var api = supertest("http://localhost:3000")
 
-
-// describe("what am i testing", function(){
+// API TEST SUITE
 describe("Candies", function(done){
-  //tests will be written inside this function
+
+  // TESTING GET /CANDIES
   describe("GET /candies", function(){
 
-  //it("description of test", function(){
     it("should return a 200 response", function(){
       api.get("/candies")
       .set("Accept", "application/json")
@@ -26,48 +25,93 @@ describe("Candies", function(done){
       })
     })
 
-    it("should return an object that have a field called 'name'. 'id', 'color' ", function(done){
-    api.get("/candies")
-    .set("Accept", "application/json")
-    .end(function(error, response){
-      expect(response.body[0]).to.have.property('id');
-      expect(response.body[0]).to.have.property('name');
-      expect(response.body[0]).to.have.property('color');
-      expect(response.body[0]).not.to.have.property('fer');
-      done()
+    it("should return an object that have 'id', 'name' and 'color'", function(done){
+      api.get("/candies")
+      .set("Accept", "application/json")
+      .end(function(error, response){
+        var firstCandy = response.body[0];
+        expect(firstCandy).to.have.all.keys('id', 'name', 'color');
+
+        // expect().to.have.property('id');
+        // expect(response.body[0]).to.have.property('name');
+        // expect(response.body[0]).to.have.property('color');
+        // expect(response.body[0]).not.to.have.property('fer');
+        done()
       })
     })
 
-    it("should return 4 elements in array", function(done){
-    api.get("/candies")
-    .set("Accept", "application/json")
-    .end(function(error, response){
-      expect(response.body.length).to.equal(4);
-      done()
+    it("should return only 4 elements in the array", function(done){
+      api.get("/candies")
+      .set("Accept", "application/json")
+      .end(function(error, response){
+        expect(response.body.length).to.be.equal(4);
+        done()
+      })
     })
   })
-})
 
   describe("POST /candies", function(){
     before(function(done){
-    api.post("/candies")
-    .set("Accept", "application/json")
-    .send({
-      "id": 5,
-      "name": "Lollipop",
-      "color": "Red"
-    }).end(done)
-  })
+      api.post("/candies")
+      .set("Accept", "application/json")
+      .send({
+        "id": 5,
+        "name": "Lollipop",
+        "color": "Red"
+      }).end(done)
+    })
 
-  it("should add a candy object to the collection candies and return it", function(done){
-    api.get("/candies")
-    .set("Accept", "application/json")
-    .end(function(error, response){
-      expect(response.body.length).to.equal(5);
-      done()
+    it("should add a candy object to the collection candies and return it", function(done){
+      api.get("/candies")
+      .set("Accept", "application/json")
+      .end(function(error, response){
+        expect(response.body.length).to.equal(5);
+        done()
+      })
     })
   })
 
-
+  describe("GET /candies/:id", function(){
+    it("should return an object with the right fields", function(done){
+      api.get("/candies/1")
+      .set("Accept", "application/json")
+      .end(function(error, response){
+        expect(response.body).to.have.all.keys('id', 'name', 'color');
+        done()
+      })
+    })
   })
-});
+
+  describe("DELETE /candies/:id", function(){
+    it("should delete an object and return message", function(done){
+      api.delete("/candies/1")
+      .set("Accept", "application/json")
+      .end(function(error, response){
+        expect(response.body['message']).to.equal('deleted');
+        done()
+      })
+    })
+  })
+
+  describe("PUT /candies/:id", function(){
+    before(function(done){
+      api.put("/candies/2/edit")
+      .set("Accept", "application/json")
+      .send({
+        id:  2,
+        name:  "NEW_NAME",
+        color: "NEW_COLOR"
+      }).end(done)
+    })
+
+    it("should update an object", function(done){
+      api.get("/candies/2")
+      .set("Accept", "application/json")
+      .end(function(error, response){
+        expect(response.body['name']).to.equal('NEW_NAME');
+        expect(response.body['color']).to.equal('NEW_COLOR');
+        done()
+      })
+    })
+  })
+})
